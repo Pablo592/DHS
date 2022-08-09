5270e89f361a2259c5c3dede08e26da218c460ca
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2022-08-09 08:42:46 -0300

    ejemplos practicos
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

---
+++ PracticandoPython/archivo.py
@@ -0,0 +1,109 @@
+#1° Ejemplo
+'''
+from math import pi
+
+radio = float(input("Ingrese el radio: "))
+
+sup = pi * radio ** 2
+
+print("La superficie es " + str(sup))
+'''
+
+#2° Ejemplo: if y else.
+'''
+if true:
+    a = 1;
+    b = 2;
+    c = 3;
+
+if true:
+    a = 1
+    b = 2
+    c = 3
+else:
+    d = 4             
+'''
+
+#3° Ejemplo: while y for 
+'''
+while i < n:
+    a += 1;
+
+for nombre in ['Pepe', 'Ana', 'Juan']:
+    print('Hola, {0}.'.format(nombre))
+
+for i in range(0,5):
+    sumatorio += i
+
+print(sumatoria)
+'''
+
+#4° Ejemplo: list
+'''
+x = list(range(0,5))
+'''
+
+#Ejemplo 1: Strings y listas.
+'''
+x = 'hola'
+print(x[1])
+print(x[-1])
+
+for letra in x:
+    print(x)
+'''
+
+#Ejemplo 2: Sobre cadenas.
+'''
+texto = "hola que tal"
+print(texto[6])
+print(texto[6:10])
+
+texto = list[range[0:5]]
+
+print(texto[:-5])
+print(texto[3:])
+print(texto[:])
+'''
+
+#Ejemplo 3: Arreglos o copias de listas.
+'''
+a = [1,2,3,4,5]
+b = a[:]
+'''
+
+#Ejemplo 4: append
+'''
+a = [1,2,3,4]
+a.append(5)
+'''
+
+#Ejemplo 5: referencias de listas
+'''
+a = [1,2,3]
+c = a
+''' 
+
+#Ejemplo 6: listas de diferentes tipos
+'''
+a = [1,'a',true]
+'''
+
+#Ejemplo 7: == y is
+'''
+a == b 
+a is c 
+'''
+
+#Ejemplo 8: buscar un elemento si existe
+'''
+if not elemento in conjuto:
+    conjunto.append(elemento)
+print(conjunto)    
+'''
+
+#Ejemplo 9: split y join.
+'''
+a.split()
+a.join()
+'''
--- archivo.py
+++
@@ -1,7 +0,0 @@
-from math import pi
-
-radio = float(input("Ingrese el radio: "))
-
-sup = pi * radio ** 2
-
-print("La superficie es: " + str(sup))

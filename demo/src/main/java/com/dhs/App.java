a6c0faa6f15474a1c746e5098beb1343d8570543
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2022-08-29 11:18:37 -0300

    creando proyecto maven
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

---
+++ demo/src/main/java/com/dhs/App.java
@@ -0,0 +1,13 @@
+package com.dhs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ demo/src/main/python/compiladoresVisitor.py
@@ -0,0 +1,18 @@
+# Generated from /home/pablo/Escritorio/DHS-Cursado/demo/src/main/python/compiladores.g4 by ANTLR 4.9.2
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .compiladoresParser import compiladoresParser
+else:
+    from compiladoresParser import compiladoresParser
+
+# This class defines a complete generic visitor for a parse tree produced by compiladoresParser.
+
+class compiladoresVisitor(ParseTreeVisitor):
+
+    # Visit a parse tree produced by compiladoresParser#s.
+    def visitS(self, ctx:compiladoresParser.SContext):
+        return self.visitChildren(ctx)
+
+
+
+del compiladoresParser
